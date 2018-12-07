da16fc589bce6627cf4e9c8c0a2b1cf685803e4e
maingate <josansnim@gmail.com>
2018-12-07 09:15:45 +0900

    프로젝트 생성

---
+++ JGExtensionExample/JGExtensionExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JGExtensionExample
+//
+//  Created by JungMoon-Mac on 07/12/2018.
+//  Copyright © 2018 JungMoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ JGExtensionExample/JGExtensionExampleTests/JGExtensionExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  JGExtensionExampleTests.swift
+//  JGExtensionExampleTests
+//
+//  Created by JungMoon-Mac on 07/12/2018.
+//  Copyright © 2018 JungMoon. All rights reserved.
+//
+
+import XCTest
+@testable import JGExtensionExample
+
+class JGExtensionExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
